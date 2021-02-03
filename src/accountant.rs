8f4b4ecce434110825509f7a0707fdad79a7cda3
Konstantin Senkevich <konstsen@gmail.com>
2021-02-04 00:13:53 +0300

    Implement accountant.

--- src/accountant.rs
+++ src/accountant.rs
@@ -1,3 +1,5 @@
+use agnostic::coin::CoinConverter;
+
 pub struct Accountant<TConnector>
 {
     private_client: std::sync::Arc<btc_sdk::client::BTCClient<TConnector>>,
@@ -21,17 +23,74 @@ where
     TConnector: hyper::client::connect::Connect + Sync + Send + Clone + 'static
 {
     fn ask(&self, coin: agnostic::coin::Coin) -> agnostic::market::Future<Result<agnostic::currency::Currency, String>> {
-        todo!()
+        let private_client = self.private_client.clone();
+        let converter = crate::CoinConverter::default();
+        let future = async move {
+            let balance = match private_client.get_trading_balance().await {
+                Some(balance) => balance,
+                None => return Err("FAILED TO GET TRADING BALANCE".to_owned()),
+            };
+            let coin_as_string = converter.to_string(coin.clone());
+            balance.into_iter().find_map(|currency| {
+                if currency.currency != coin_as_string {
+                    return None;
+                }
+                let currency = btc_sdk::models::typed::Currency::from(currency);
+                Some(agnostic::currency::Currency {
+                    coin: coin.clone(),
+                    amount: currency.available,
+                    held: currency.reserved,
+                })
+            });
+            unimplemented!()
+        };
+        Box::pin(future)
     }
 
     fn ask_both(
         &self,
         coins: agnostic::coin::CoinPair,
     ) -> agnostic::market::Future<Result<(agnostic::currency::Currency, agnostic::currency::Currency), String>> {
-        todo!()
+        let private_client = self.private_client.clone();
+        let converter = crate::CoinConverter::default();
+        let future = async move {
+            let balance = match private_client.get_trading_balance().await {
+                Some(balance) => balance,
+                None => return Err("FAILED TO GET TRADING BALANCE".to_owned()),
+            };
+            let sell_coin_as_string = converter.to_string(coins.sell.clone());
+            let left_coin = balance.iter().find_map(|currency| {
+                if currency.currency != sell_coin_as_string {
+                    return None;
+                }
+                let currency = btc_sdk::models::typed::Currency::from(currency.clone());
+                Some(agnostic::currency::Currency {
+                    coin: coins.sell.clone(),
+                    amount: currency.available,
+                    held: currency.reserved,
+                })
+            });
+            let buy_coin_as_string = converter.to_string(coins.buy.clone());
+            let right_coin = balance.iter().find_map(|currency| {
+                if currency.currency != buy_coin_as_string {
+                    return None;
+                }
+                let currency = btc_sdk::models::typed::Currency::from(currency.clone());
+                Some(agnostic::currency::Currency {
+                    coin: coins.buy.clone(),
+                    amount: currency.available,
+                    held: currency.reserved,
+                })
+            });
+            match (left_coin, right_coin) {
+                (Some(left), Some(right)) => Ok((left, right)),
+                _ => Err("Failed to find coins in wallet".to_owned()),
+            }
+        };
+        Box::pin(future)
     }
 
-    fn calculate_volume(&self, coins: agnostic::coin::CoinPair, price: f64, amount: f64) -> f64 {
-        todo!()
+    fn calculate_volume(&self, _coins: agnostic::coin::CoinPair, price: f64, amount: f64) -> f64 {
+        price * amount
     }
 }
